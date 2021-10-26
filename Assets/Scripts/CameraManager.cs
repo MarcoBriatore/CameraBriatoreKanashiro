899b459ea9e0dd45a5976f65fdecd19d2ee7ecb6
Marco Briatore <30911931+MarcoBriatore@users.noreply.github.com>
2021-10-26 13:52:22 -0300

    Added textures

--- Assets/Scripts/CameraManager.cs
+++ Assets/Scripts/CameraManager.cs
@@ -6,30 +6,36 @@ using UnityEngine;
 public class CameraManager : MonoBehaviour
 {
     public GameObject[] cameras;
-    
+    private AudioManager audioManager;
+
+    public void Awake()
+    {
+        audioManager = FindObjectOfType<AudioManager>();
+    }
     // Update is called once per frame
     void Update()
     {
         
-        if (Input.GetKeyDown(KeyCode.F1))
+        if (!cameras[0].activeSelf && Input.GetKeyDown(KeyCode.F1))
         {
             cameras[0].SetActive(true);
             cameras[1].SetActive(false);
             cameras[2].SetActive(false);
+            audioManager.Play("Ready");
         }
-        else if (Input.GetKeyDown(KeyCode.F2))
+        else if (!cameras[1].activeSelf && Input.GetKeyDown(KeyCode.F2))
         {
-
             cameras[0].SetActive(false);
             cameras[1].SetActive(true);
             cameras[2].SetActive(false);
+            audioManager.Play("Go");
         }
-        else if (Input.GetKeyDown(KeyCode.F3))
+        else if (!cameras[2].activeSelf && Input.GetKeyDown(KeyCode.F3))
         {
             cameras[0].SetActive(false);
             cameras[1].SetActive(false);
             cameras[2].SetActive(true);
-
+            audioManager.Play("Complete");
         }
     }
 }
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,23 +4,12 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-
-    private Animator animator;
+    [SerializeField]
     private float speed = 10;
+    [SerializeField]
     private float rotationSpeed = 720;
     private AudioManager audioManager;
-
-    private void Awake()
-    {
-
-        audioManager = FindObjectOfType<AudioManager>(); ;
-    }
-
-    void Start()
-    {
-        animator = GetComponent<Animator>();
-        audioManager.Play("Ready");
-    }
+    
 
     // Update is called once per frame
     void Update()
@@ -28,32 +17,12 @@ public class PlayerController : MonoBehaviour
         float horizontal = Input.GetAxis("Horizontal");
         float vertical = Input.GetAxis("Vertical");
         Movement(horizontal, vertical);
-        Animation(horizontal, vertical);
     }
 
     void Movement(float horizontal, float vertical)
     {
         Vector3 movementDirection = new Vector3(horizontal, 0, vertical);
         transform.Translate(speed * movementDirection.normalized * Time.deltaTime, Space.World);
-
-        if(movementDirection != Vector3.zero)
-        {
-            Quaternion toRotation = Quaternion.LookRotation(movementDirection, Vector3.up);
-            transform.rotation = Quaternion.RotateTowards(transform.rotation, toRotation, rotationSpeed * Time.deltaTime);
-        }
-    }
-
-    void Animation(float horizontal, float vertical)
-    {
-        if (Input.GetAxis("Horizontal") == 0 && Input.GetAxis("Vertical") == 0)
-        {
-            animator.SetBool("Idle", true);
-        }
-        else
-        {
-            audioManager.Play("Go");
-            animator.SetBool("Idle", false);
-        }
     }
     
 }
