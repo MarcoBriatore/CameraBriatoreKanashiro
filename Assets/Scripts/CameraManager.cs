358befacd8a8af0d302e923b3d7d9460741dee0b
Marco Briatore <30911931+MarcoBriatore@users.noreply.github.com>
2021-10-26 00:02:34 -0300

    Created Camera and Audio Manager

---
+++ Assets/Scripts/CameraManager.cs
@@ -0,0 +1,35 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraManager : MonoBehaviour
+{
+    public GameObject[] cameras;
+    
+    // Update is called once per frame
+    void Update()
+    {
+        
+        if (Input.GetKeyDown(KeyCode.F1))
+        {
+            cameras[0].SetActive(true);
+            cameras[1].SetActive(false);
+            cameras[2].SetActive(false);
+        }
+        else if (Input.GetKeyDown(KeyCode.F2))
+        {
+
+            cameras[0].SetActive(false);
+            cameras[1].SetActive(true);
+            cameras[2].SetActive(false);
+        }
+        else if (Input.GetKeyDown(KeyCode.F3))
+        {
+            cameras[0].SetActive(false);
+            cameras[1].SetActive(false);
+            cameras[2].SetActive(true);
+
+        }
+    }
+}
